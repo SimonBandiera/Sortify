8ef0fee0ba4a6f8145e3c07ae4f8cf697b027101
HForGames <hugo.galan8@gmail.com>
2022-05-15 23:37:54 +0200

    [*] adding gunicorn to server and thread gestion

--- Server/api/spotify/api.py
+++ Server/api/spotify/api.py
@@ -5,17 +5,19 @@ from time import time
 
 BASE_URL = "http://localhost:5000"
 
+
 def update_token(token):
     session["access_token"] = token["access_token"]
     session["expires_in"] = int(token["expires_in"] + time())
     session["refresh_token"] = token["refresh_token"]
     return
 
+
 def get_access_token(code):
     param = {
-        'code' : code,
-        'redirect_uri' : f"{BASE_URL}/spotify/callback",
-        'grant_type' : 'authorization_code'
+        'code': code,
+        'redirect_uri': f"{BASE_URL}/spotify/callback",
+        'grant_type': 'authorization_code'
     }
     r = requests.post('https://accounts.spotify.com/api/token', params=param, headers=header_basic)
     print(r.json())
@@ -23,16 +25,18 @@ def get_access_token(code):
         return {}
     return r.json()
 
+
 def refresh_access_token(refresh_token):
     param = {
-        'refresh_token' : refresh_token,
-        'grant_type' : 'refresh_token'
+        'refresh_token': refresh_token,
+        'grant_type': 'refresh_token'
     }
     r = requests.post('https://accounts.spotify.com/api/token', params=param, headers=header_basic)
     print(r.json())
     if r.status_code != 200:
         return {}
     return r.json()
+
 
 def get_user_playlist():
     session["next_dashboard"] = None
@@ -42,10 +46,12 @@ def get_user_playlist():
         if token == {}:
             return {}
         update_token(token)
-    bearer = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-              'Accept': 'application/json',
-              'Content-Type' : 'application/json',
-              'Authorization' : f'Bearer {session["access_token"]}'}
+    bearer = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {session["access_token"]}'
+    }
     r = requests.get("https://api.spotify.com/v1/me/playlists", headers=bearer)
     if r.status_code != 200:
         return {}
@@ -53,3 +59,20 @@ def get_user_playlist():
     session["next_dashboard"] = value["next"]
     session["previous_dashboard"] = value["previous"]
     return r.json()
+
+
+def get_playlist_track(playlist_id, session):
+        if session["expires_in"] <= time():
+            token = refresh_access_token()
+            if token == {}:
+                return {}
+            update_token(token)
+        param = {"field": "items(track(name%2Chref%2Cartists(name)))%2Ctotal%2Climit"}
+        bearer = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {session["access_token"]}'
+        }
+        r = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", params=param, header=bearer)
+        print(r.json())
--- Server/main.py
+++ Server/main.py
@@ -1,13 +1,31 @@
 import urllib
-from flask import Flask, session, render_template, url_for, request, redirect
-from api.spotify.api import get_access_token, refresh_access_token, update_token, get_user_playlist
+from flask import Flask, session, render_template, url_for, request, redirect, sessions
+from api.spotify.api import get_access_token, refresh_access_token, update_token, get_user_playlist, get_playlist_track
 from api.spotify.key import ClientSecret, ClientID
+from flask_socketio import SocketIO, emit, send
 import secrets
-import pprint
+from threading import Thread
+import queue
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex()
+socketio = SocketIO(app, manage_session=False, async_mode='threading', cors_allowed_origins="*")
 BASE_URL = "http://localhost:5000"
+
+
+def thread_test(q):
+    while True:
+        playlist_id = q.get()
+        get_playlist_track(playlist_id, session)
+        with app.test_request_context('/sort/' + playlist_id):
+            socketio.emit("finish")
+        print(f"finish {playlist_id}")
+        q.task_done()
+
+
+queue = queue.Queue()
+worker = Thread(target=thread_test, args=(queue,), daemon=True)
+worker.start()
 
 
 @app.route('/')
@@ -37,15 +55,17 @@ def dashboard():
     user_playlist = get_user_playlist()
     if user_playlist == {}:
         return render_template("error.html", error="Request error")
-    pprint.pprint(user_playlist)
     have_next = session["next_dashboard"] is not None
     have_previous = session["previous_dashboard"] is not None
     return render_template("dashboard.html", user_playlist=user_playlist, next=have_next, previous=have_previous,
-                           no_playlist=len(user_playlist["items"]) == 0)
+                           no_playlist=len(user_playlist["items"]) == 0, )
+
 
 @app.route("/sort/<playlist_id>")
 def sort(playlist_id):
-    return playlist_id
+    queue.put(playlist_id)
+    return render_template("sort.html")
+
 
 @app.route("/logout")
 def logout():
@@ -54,8 +74,9 @@ def logout():
     session.pop("refresh_token", None)
     session.pop("next_dashboard", None)
     session.pop("previous_dashboard", None)
+    session.pop("thread", None)
     return redirect("/")
 
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    socketio.run(app)
