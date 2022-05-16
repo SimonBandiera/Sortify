f575d53d915fd914fc931bbe1576b87e7d088325
simon_maison <simon.bandiera@epitech.eu>
2022-05-16 15:24:21 +0200

    [+] setup tailwindcss

---
+++ Server/static/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["../templates/index.html",
+            "../templates/dashboard.html",
+            "../templates/error.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
