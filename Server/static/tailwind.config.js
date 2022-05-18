f2a107bafdcac94145ce016b542fc9ef75f51c61
Simon <simon.bandiera@epitech.eu>
2022-05-18 19:01:35 +0200

    [+] advance on loading screen

--- Server/static/tailwind.config.js
+++ Server/static/tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   content: ["../templates/index.html",
             "../templates/dashboard.html",
-            "../templates/error.html"],
+            "../templates/error.html",
+            "../templates/sort.html",
+            "../templates/test.html",
+            "../templates/create.html"],
   theme: {
-    extend: {},
+    extend: {
+        transitionProperty: {
+            'width': 'width'
+        },
+    },
   },
   plugins: [],
 }
